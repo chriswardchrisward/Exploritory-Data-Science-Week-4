850a8e125083cd9bc2baa1e37a3a45232aa46141
Chris Ward <chriswardchrisward@gmail.com>
2017-10-03 13:32:42 -0700

    Added plot2.R and plot2.png

---
+++ Plot2.R
@@ -0,0 +1,19 @@
+plot2 <- function(){
+  ## Read in Data if it hasn't been done yet
+  if(!exists("NEI")){
+    NEI <- readRDS("./data/summarySCC_PM25.rds")
+  }
+  if(!exists("SCC")){
+    SCC <- readRDS("./data/Source_Classification_Code.rds")
+  }
+  
+  BmoreNEI <- NEI[NEI$fips=="24510",]
+  BmoreEmissions <- tapply(BmoreNEI$Emissions, BmoreNEI$year, sum)
+  
+  
+  
+  png("plot2.png",width=480,height=480,units="px",bg="transparent")
+  barplot(BmoreEmissions, xlab = "Year", ylab = "Total Emission (tons)", 
+          main = "Total Emissions per year in Baltimore City")
+  dev.off()
+}
