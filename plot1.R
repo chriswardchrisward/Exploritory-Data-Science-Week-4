67e0334fa80d4eb26fd679c60cfae8675a7367b1
Chris Ward <chriswardchrisward@gmail.com>
2017-10-03 13:22:09 -0700

    plot1.R and plot1.png finished

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
-## This first line will likely take a few seconds. Be patient!
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
-
-totEmissions <- tapply(NEI$Emissions, NEI$year, sum)
-
-
-png(filename = "plot1.png", width = 480, height = 480, units = "px", bg = "white")
-par(mfrow = c(1,1), mar = c(4,5,2.5,2))
-barplot(totEmissions, xlab = "Year", ylab = "Total Emission (tons)", 
-        main = "Total Emissions per year")
-dev.off()
-
+plot1 <- function(){
+  ## This first line will likely take a few seconds. Be patient!
+  NEI <- readRDS("summarySCC_PM25.rds")
+  SCC <- readRDS("Source_Classification_Code.rds")
+  
+  totEmissions <- tapply(NEI$Emissions, NEI$year, sum)
+  
+  
+  png("plot1.png",width=480,height=480,units="px",bg="transparent")
+  barplot(totEmissions, xlab = "Year", ylab = "Total Emission (tons)", 
+          main = "Total Emissions per year")
+  dev.off()
+}
